34aafbec82e447146a11de989a073a5d06aa7fb4
Yash <eternity7678457@gmail.com>
2020-02-20 12:11:56 +0530

    Intial

--- app/src/main/java/com/example/mybook/loginActivity.java
+++ app/src/main/java/com/example/mybook/loginActivity.java
@@ -29,6 +29,7 @@ public class loginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // hello
         setContentView(R.layout.activity_login);
         mFirebaseAuth = FirebaseAuth.getInstance();
         emailId = findViewById(R.id.editText);
